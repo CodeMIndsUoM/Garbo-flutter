8b7ebe5111d6567c5f232622963335c0817d2e85
wijewardana S.J <wijewardanasj.23@uom.lk>
2026-02-06 16:10:04 +0530

    Create Collection Team dashbord initial page

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:garbo_swms/presentation/login.dart';
 import 'package:garbo_swms/presentation/citizen/pages/home_page.dart';
+import 'package:garbo_swms/presentation/collection_team/pages/dashboard.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -9,11 +10,13 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Garbo SWMS',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.green, fontFamily: 'Arimo'),
-      initialRoute: '/citizen-home', // Temporary for testing
+      initialRoute: '/collector/dashboard', // Temporary for testing
       routes: {
         '/login': (context) => Login(),
         '/citizen-home': (context) => CitizenHomePage(),
+        '/collector/dashboard': (context) => CollectionTeamDashboard(),
       },
     );
   }
