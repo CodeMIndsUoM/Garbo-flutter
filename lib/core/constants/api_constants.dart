04040c8f58bfc5dfe0cd5c9c50f0e6be912c8668
wijewardana S.J. <wijewardanasj.23@uom.lk>
2026-02-25 15:24:08 +0530

    feat(api): add ApiService and constants to connect to Garbo backend

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,8 @@
+class ApiConstants {
+  // Use 10.0.2.2 for Android Emulator, 127.0.0.1 for iOS Simulator
+  static const String baseUrl = 'http://127.0.0.1:8081/api';
+
+  // Field Mentor Endpoints
+  static const String fieldMentors = '/fieldmentors';
+  static const String bins = '/bins';
+}
